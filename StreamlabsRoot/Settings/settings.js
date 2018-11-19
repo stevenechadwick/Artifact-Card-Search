607b93c6e79d4ddd10faa2c0c99c2941abbf8571
stevechadwick <39637908+stevechadwick@users.noreply.github.com>
2018-11-18 22:01:04 -0500

    full fileset

---
+++ StreamlabsRoot/Settings/settings.js
@@ -0,0 +1,13 @@
+﻿var settings = {
+  "CardName": true,
+  "CardType": true,
+  "CardText": true,
+  "Rarity": false,
+  "Colour": true,
+  "Attack": true,
+  "Armour": true,
+  "Health": true,
+  "Ability": false,
+  "SignatureCard": true,
+  "Cost": true
+};
